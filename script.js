5e21bed9fe0cc78fe61f0b17664ce82fd5a366aa
Phearcet <youngtsai89@gmail.com>
2025-03-09 23:00:35 +0800

    Added a broken resolution button
    Added a button prompt for resolution but it's broken right now.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.querySelector("#container");
+const resBtn = document.querySelector("#btn");
+let resolution = 16;
 
-for (let i = 0; i < 256; i++) {
+resBtn.addEventListener('click',() => {
+  let resolution = Number(window.prompt("Enter New Canvas Resolution", 16));
+  console.log(resolution*resolution);
+});
+
+for (let i = 0; i < resolution*resolution; i++) {
   const div = document.createElement("div");
   
   div.style.width = "20px";
