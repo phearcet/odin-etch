b9e50e3e168269eb78a5de21b46b85e307334682
Phearcet <youngtsai89@gmail.com>
2025-03-10 01:13:23 +0800

    Fixed grid format
    Fixed the issue of blocks not displaying in a square format. but the size of block issue still persisted.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resBtn = document.querySelector("#btn");
 let resolution = 16;
 
 resBtn.addEventListener('click',() => {
-  let resolution = Number(window.prompt("Enter New Canvas Resolution (lower than 100)", 16));
+  let resolution = Number(window.prompt("Enter New Canvas Resolution (lower than 100)", '16'));
   if(resolution > 100) {
     alert("Error. Please enter a number under 100");
   };
@@ -12,19 +12,32 @@ resBtn.addEventListener('click',() => {
   drawDiv(resolution);
 });
 function drawDiv(resolution){
-  for (let i = 0; i < resolution*resolution; i++) {
-    const div = document.createElement("div");
-    
-    div.style.width = "20px";
-    div.style.height = "20px";
-    div.style.backgroundColor = "white";
-    div.style.border = "solid 1px";
-    div.onmouseenter = (Event) => {
-      div.style.backgroundColor = "darkgrey";
+  for (let i = 0; i < resolution; i++) {
+
+    let row = document.createElement("div");
+    row.classList.add("grid");
+
+    for(let i = 0; i < resolution; i++){
+
+      const div = document.createElement("div");
+      div.classList.add("grid");
+      div.style.width = "20px";
+      div.style.height = "20px";
+      
+      div.style.backgroundColor = "white";
+      div.style.border = "solid 1px";
+      div.onmouseenter = (Event) => {
+        div.style.backgroundColor = "darkgrey";
     } ;
+
+      row.appendChild(div);
+    }
+  row.style.display = "flex";
+
+
     
-    container.appendChild(div);
+  container.appendChild(row);
 }
 }
 
-drawDiv();
+drawDiv(resolution);
