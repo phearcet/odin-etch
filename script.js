89a16cc96444cd286ccdad45e9b8f61dbb61d1f6
Phearcet <youngtsai89@gmail.com>
2025-03-09 22:06:13 +0800

    Added hover Eventlistener
    Added the mouse over eventlistener but the div cannot be refered outside the for function.

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
-  //div.textContent = "h";
+  
   div.style.width = "20px";
   div.style.height = "20px";
-  div.style.backgroundColor = "grey";
+  div.style.backgroundColor = "lightgrey";
+  div.onmouseenter = mouseHover;
   container.appendChild(div);
+}
+
+function mouseHover() {
+  div.style.backgroundColor = "darkgrey";
+  
 }
