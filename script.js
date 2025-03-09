45a17b1d7f5ced0b7ccf467c73c8a6e1b3a0cfd8
Phearcet <youngtsai89@gmail.com>
2025-03-09 12:40:18 +0800

    Added 16*16 grid
    Styled the div with DOM. Used flex box to make it a 16 by 16 gird.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   //div.textContent = "h";
+  div.style.width = "20px";
+  div.style.height = "20px";
+  div.style.backgroundColor = "grey";
   container.appendChild(div);
 }
