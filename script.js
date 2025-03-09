de6aff8af9d94ceeb4e26fd8e2834189c6c1c701
Phearcet <youngtsai89@gmail.com>
2025-03-10 00:22:30 +0800

    Fix button function
    Entering a new value now erase the old canvas and draws a new one. But the canvas size is still messed up.

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ const resBtn = document.querySelector("#btn");
 let resolution = 16;
 
 resBtn.addEventListener('click',() => {
-  let resolution = Number(window.prompt("Enter New Canvas Resolution", 16));
+  let resolution = Number(window.prompt("Enter New Canvas Resolution (lower than 100)", 16));
+  if(resolution > 100) {
+    alert("Error. Please enter a number under 100");
+  };
   console.log(resolution*resolution);
+  container.innerHTML = '';
+  drawDiv(resolution);
 });
-
-for (let i = 0; i < resolution*resolution; i++) {
-  const div = document.createElement("div");
-  
-  div.style.width = "20px";
-  div.style.height = "20px";
-  div.style.backgroundColor = "white";
-  div.style.border = "solid 1px";
-  div.onmouseenter = (Event) => {
-    div.style.backgroundColor = "darkgrey";
-  } ;
-  
-  container.appendChild(div);
+function drawDiv(resolution){
+  for (let i = 0; i < resolution*resolution; i++) {
+    const div = document.createElement("div");
+    
+    div.style.width = "20px";
+    div.style.height = "20px";
+    div.style.backgroundColor = "white";
+    div.style.border = "solid 1px";
+    div.onmouseenter = (Event) => {
+      div.style.backgroundColor = "darkgrey";
+    } ;
+    
+    container.appendChild(div);
+}
 }
 
+drawDiv();
